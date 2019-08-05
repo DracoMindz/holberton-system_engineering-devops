feac6c88b6d416340e1325a2518ed990ae0838bd
DracoMindz <DracoMindz@github.com>
2019-08-05 20:13:15 +0000

    Puppet kill process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,4 @@
 #manifest kills process
 exec { "killmenow":
-    path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-    command => "ps -ef | grep nrpe | grep -v pts/1 | /bin/awk '{print \$2}' | xargs pkill -9",
-    provider => 'shell',
-    subscribe => File["/root/Backup/deploy"],
-    refreshonly => true,
+        command => "/usr/bin/pkill -x killmenow",
 }
