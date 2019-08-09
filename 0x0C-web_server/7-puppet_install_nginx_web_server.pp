ddae1f6a0fc217ede3ac6c50eb60042c7d53a1fb
DracoMindz <DracoMindz@github.com>
2019-08-09 23:42:49 +0000

    Puppet Install nginx

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,23 @@
+#install and configuration of nginx with puppet
+# redirection included
+package { 'nginx':
+  ensure      => installed,
+  listen-port => 80,
+}
+
+file { '/var/www/html/index.html':
+    content => 'Holberton School',
+}
+
+file_line { 'snoopy':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _',
+  line   => 'location /redirect_me {\nrewrite ^/(.*)$https://www.youtube.com/watch?v=WZ9W0nIonnM\n\t}";permanent;',
+}
+
+
+service { 'nginx':
+  ensure  => 'running',
+  require => Package['nginx']
+}
