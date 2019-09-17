a3b7e90532c2f3496d5191db818099f983bdbf76
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-09-16 22:37:23 -0400

    Update: syntax

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,11 +2,11 @@
 """ Python script: uses REST API, for employee, return info about """
 """ employee TODO list progress """
 
-import requests
-import sys
 
 if __name__ == "__main__":
-
+    import requests
+    import sys
+  
     """ employee information requests """
     employee = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
                             format(sys.argv[1])).json()
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     tcom_tasks = len(com_tasks)
 
     """ Print the employee task data """
-    print('Employee {} is done with tasks ({}/{}):'.
+    print('Employee {} is done with tasks({}/{}):'.
           format(employee_name, tcom_tasks, tot_tasks))
     for task in com_tasks:
         print('\t {}'.format(task.get('title')))
