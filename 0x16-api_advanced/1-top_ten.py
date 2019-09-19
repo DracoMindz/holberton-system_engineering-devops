6022a6e82b2d8e0d8922eeaac7d97b398e43ea22
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-09-19 19:47:23 -0400

    API: added print none

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 
 import requests
+import json
 
 
 def top_ten(subreddit):
     '''
-    GET/print top ten titlesof first ten hot listings
+    GET/print top ten titles of first ten hot listings
     '''
 
     try:
-        request = requests.get('https://www.reddit.com/r/{}/hot.json?limit=10'.
-                               format(subreddit), allow_redirects=False,
+        request = requests.get('https://www.reddit.com/r/{}/hot.json?limit=10'
+                               .format(subreddit), allow_redirects=False,
                                headers={'User-Agent': 'Betty'}).json()
         for m in request['data']['children']:
             print(m['data']['title'])
     except BaseException:
-        return ('None')
+        print('None')
