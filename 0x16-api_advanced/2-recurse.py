40905dce6ed5ba449d52486bd832bf0f4d5f916b
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-09-19 21:43:19 -0400

    API: recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,15 +8,16 @@ def recurse(subreddit, hot_list=[]):
     GET list of all hot articles use a recursive
     '''
 
-    if len(hot_list) == 0:
+    if not hot_list:
         return None
     try:
         req = requests.get('https://www.reddit.com/r/{}/hot.json?after=after'
                            .format(subreddit), allow_redirects=False,
                            headers={'User-Agent': 'Betty'}).json()
+
         hotAfter = req['data']['after']
         for m in req['data']['children']:
             hot_list.append(m['data']['title'])
         return recurse(subreddit, hot_list, hotAfter)
-    except BaseException:
-        print(None)
+    except KeyError:
+        return None
