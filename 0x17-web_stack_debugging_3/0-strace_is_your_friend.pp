e41f2b2e8f005967e1edeb03eb5f46b8bbf1def6
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-10-07 12:04:49 -0400

    Update: puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Fix Apache then automate with puppet
+# Fix Apache then automate with puppet include path
 exec {'replace phpp with php'
   command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   path => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
