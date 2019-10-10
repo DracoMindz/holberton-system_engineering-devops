6979844839639bedc7474777036efeb76baaa964
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-10-10 13:56:41 -0400

    Update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Script increases the limit of requests allowed nginx server
-exec{ 'limit_requests_increase':
+exec { 'limit_requests_increase':
 command => 'sed -i "s/15/30000/g" /etc/default/nginx; sudo service nginx restart',
-path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  }
+path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin']
+}
